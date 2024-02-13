08c985a9c0e2e88a3edae2f24f8237107a3f6213
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2024-02-13 09:58:24 -0500

    Ajout de commentaires

--- src/testsUnitaires/TestFileChainee.java
+++ src/testsUnitaires/TestFileChainee.java
@@ -5,15 +5,14 @@ import structures.FileChainee;
 public class TestFileChainee {
 
     public static void main(String[] args) {
-        // Appel à la méthode de test de la file chaînée
         testFileChainee();
     }
 
-    // Méthode de test pour la file chaînée
     public static void testFileChainee() {
         // Création d'une file chaînée d'entiers
         FileChainee<Integer> file = new FileChainee<>();
 
+        // Test de la méthode estVide après la création de la file
         System.out.println("La file est vide ? " + file.estVide()); // Devrait afficher true
 
         // Test d'ajout d'éléments
@@ -21,20 +20,20 @@ public class TestFileChainee {
         file.ajouterElement(20);
         file.ajouterElement(30);
 
-        // Test de la méthode estVide
+        // Test de la méthode estVide après avoir ajouté des éléments
         System.out.println("La file est vide ? " + file.estVide()); // Devrait afficher false
 
         // Test de la méthode enleverElement
         System.out.println("Élément enlevé : " + file.enleverElement()); // Devrait afficher 10
         System.out.println("Élément enlevé : " + file.enleverElement()); // Devrait afficher 20
 
-        // Test après enlèvement d'éléments
+        // Test après avoir enlevé deux des trois éléments
         System.out.println("La file est vide ? " + file.estVide()); // Devrait afficher false
 
-        // Test d'enlèvement d'un dernier élément
+        // Test d'enlèvement du dernier élément
         System.out.println("Élément enlevé : " + file.enleverElement()); // Devrait afficher 30
 
-        // Test après enlèvement de tous les éléments
+        // Test après avoir enlevé tous les éléments
         System.out.println("La file est vide ? " + file.estVide()); // Devrait afficher true
 
         // Tentative d'enlever un élément d'une file vide (devrait lever une exception)
