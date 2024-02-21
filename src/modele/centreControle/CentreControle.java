823fd08dea49702c7d50e6c872682d640633b106
Leonard Marcoux <leonard.marcoux03@gmail.com>
2024-02-21 10:37:08 -0500

    creation des classes

---
+++ src/modele/centreControle/CentreControle.java
@@ -0,0 +1,18 @@
+package modele.centreControle;
+
+import modele.communication.TransporteurMessage;
+import modele.communication.Message;
+
+public class CentreControle extends TransporteurMessage {
+    // Implémentation de la méthode abstraite pour CentreControle
+    @Override
+    protected void envoyerMessage(Message msg) {
+        // too complete
+    }
+
+    // Implémentation de la méthode abstraite pour CentreControle
+    @Override
+    protected void gestionnaireMessage(Message msg) {
+        // too complete
+    }
+}
