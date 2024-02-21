461f8787140aea532c68758db985886a171f8826
Leonard Marcoux <140647944+AdelnorAcmorux@users.noreply.github.com>
2024-02-21 11:39:57 -0500

    creation des classes (#4)

---
+++ src/modele/rover/Rover.java
@@ -0,0 +1,18 @@
+package modele.rover;
+
+import modele.communication.TransporteurMessage;
+import modele.communication.Message;
+
+public class Rover extends TransporteurMessage {
+    // Implémentation de la méthode abstraite pour Rover
+    @Override
+    protected void envoyerMessage(Message msg) {
+        // too complete
+    }
+
+    // Implémentation de la méthode abstraite pour Rover
+    @Override
+    protected void gestionnaireMessage(Message msg) {
+        // too complete
+    }
+}
