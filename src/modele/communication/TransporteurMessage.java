6dd6188d9f88ece15c16dbdd396a9aab19e24cc7
Leonard Marcoux <leonard.marcoux03@gmail.com>
2024-03-05 15:22:49 -0500

    rajout de commentaires

--- src/modele/communication/TransporteurMessage.java
+++ src/modele/communication/TransporteurMessage.java
@@ -94,9 +94,9 @@ public abstract class TransporteurMessage extends Thread {
         while (true) {
             lock.lock();
             try {
-                Message msg = fileMessages.poll();
+                Message msg = fileMessages.poll(); //recupere le dernier message de la file
 
-                if (msg == null || nackEnvoye) {
+                if (msg == null || nackEnvoye) {   //si la file est vide, ou un cacj a ete envoyer, aucune action necessaire
                     break;
                 }
 
@@ -105,9 +105,11 @@ public abstract class TransporteurMessage extends Thread {
                     Nack nack = (Nack) msg;
                     int compteManquant = nack.getCompte();
 
+                    //parcour la liste des message
                     while (!messagesEnvoyes.isEmpty()) {
                         Message messageEnvoye = messagesEnvoyes.peek();
 
+                        // quand on trouve le message manquant, on delete tt les message d'apres
                         if (messageEnvoye instanceof Nack || messageEnvoye.getCompte() < compteManquant) {
                             messagesEnvoyes.poll();
                         } else {
@@ -118,15 +120,16 @@ public abstract class TransporteurMessage extends Thread {
                     // Message à envoyer
                     Message messageAEnvoyer = messagesEnvoyes.peek();
                     envoyerMessage(messageAEnvoyer);
-                    messagesRecus.remove(nack);
+                    messagesRecus.remove(nack);    // on eneleve le nack
                 } else if (msg.getCompte() != compteCourant) {
+                    //renvoie un nack qui dit le nombre de message manquant
                     envoyerMessage(new Nack(compteCourant));
                     nackEnvoye = true;
                 } else if (msg.getCompte() < compteCourant) {
                     System.out.println("Message dupliqué rejeté : " + msg);
                 } else {
                     gestionnaireMessage(msg);
-                    fileMessages.poll();
+                    fileMessages.poll();          //Can you comment this part?!
                     compteCourant++;
                 }
             } finally {
