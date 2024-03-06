111099debb8530ebcb1751f7cf9dbc9a199374e2
Leonard Marcoux <leonard.marcoux03@gmail.com>
2024-03-06 11:28:59 -0500

    Ajout des reference au relais satellite, centre control et rover

--- src/modele/centreControle/CentreControle.java
+++ src/modele/centreControle/CentreControle.java
@@ -2,17 +2,25 @@ package modele.centreControle;
 
 import modele.communication.TransporteurMessage;
 import modele.communication.Message;
+import modele.satelliteRelai.SatelliteRelai;
 
 public class CentreControle extends TransporteurMessage {
+    private SatelliteRelai relaisSatellite;
+
+    public CentreControle(SatelliteRelai relaiSatelitte) {
+        this.relaisSatellite = relaiSatelitte;
+    }
+
     // Implémentation de la méthode abstraite pour CentreControle
     @Override
     protected void envoyerMessage(Message msg) {
-        // too complete
+        relaisSatellite.envoyerMessageVersRover(msg);
+        messagesEnvoyes.add(msg);
     }
 
     // Implémentation de la méthode abstraite pour CentreControle
     @Override
     protected void gestionnaireMessage(Message msg) {
-        // too complete
+        System.out.println("Centre de controle: Messafe reçu - " + msg.getNumero());
     }
 }
